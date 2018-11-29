96be0f7f1f2d5494500fffef5651ad6dc90a88e7
Sridhar Madala <Si.madala19@gmail.com>
2018-11-28 17:04:31 -0800

    Completed fake sign in and sign out

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { userSignIn, userSignOut } from '../actions'
 
 class Nav extends Component {
+    renderLinks(){
+        const { auth, signIn, signOut } = this.props;
+        if(auth){
+            return <button onClick={signOut} className="yellow btn">Sign Out</button>
+        }
+
+        return <button onClick={signIn} className="btn red">Sign In</button>
+    }
+
     render(){
         const navStyle = {
             padding: '0 8px'
         }
+        console.log('User Auth:', this.props.auth);
 
         return (
             <nav style={navStyle} className='green darken-4'>
@@ -28,7 +40,7 @@ class Nav extends Component {
                             <Link to="/quotes">Quotes</Link>
                         </li>
                         <li>
-                            <Link to="/sign_in">Sign In</Link>
+                            {this.renderLinks()}
                         </li>
                         <li>
                             <Link to="/sign_up">Sign Up</Link>
@@ -40,4 +52,13 @@ class Nav extends Component {
     }
 }
 
-export default Nav;
+function mapStateToProps(state){
+    return {
+        auth: state.user.auth
+    }
+}
+
+export default connect(mapStateToProps, {
+    signIn: userSignIn,
+    signOut: userSignOut
+})(Nav)
